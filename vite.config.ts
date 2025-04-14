5dea5e2756aa96838896e2797da5420c883b9e27
Hashir Akbar <hashirbusinessinfo@gmail.com>
2025-04-15 03:26:27 +0500

    added domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,30 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { join as joinPath } from 'node:path';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { join as joinPath } from "node:path";
 
 export default defineConfig({
   server: {
     port: 25533,
+    host: "skola.bwellacademy.com",
+    cors: true,
+  },
+  preview: {
+    host: "skola.bwellacademy.com",
   },
   plugins: [
     vue({
       reactivityTransform: true,
       template: {
         compilerOptions: {
-          isCustomElement: (tag) => ['mux-video'].includes(tag),
-        }
-      }
+          isCustomElement: (tag) => ["mux-video"].includes(tag),
+        },
+      },
     }),
   ],
   resolve: {
     alias: {
-      '@': joinPath(__dirname, 'src'),
+      "@": joinPath(__dirname, "src"),
     },
   },
-  envPrefix: 'APP_',
+  envPrefix: "APP_",
 });
